cfc5dc1550a1cabc17a34358292b2d60b68711da
Juan Soto <juan@juansoto.me>
2017-03-08 20:56:40 -0500

    Add difference list implementation
    Adds a started implementation of difference lists with tests.

--- lib/dlist.ex
+++ lib/dlist.ex
@@ -1,18 +1,40 @@
 defmodule DList do
-  @moduledoc """
-  Documentation for DList.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  def from_list(xs) do
+    fn ys -> xs ++ ys end
+  end
 
-  ## Examples
+  def to_list(dlist) do
+    dlist.([])
+  end
 
-      iex> DList.hello
-      :world
+  def empty, do: from_list([])
 
-  """
-  def hello do
-    :world
+  def append(dlist_a, dlist_b) do
+    Utils.compose(dlist_a, dlist_b)
+  end
+
+  def cons(dlist, x) do
+    Utils.compose(Utils.list_cons(x), dlist)
+  end
+
+  def snoc(dlist, x) do
+    Utils.compose(dlist, Utils.list_cons(x))
+  end
+
+  def head(dlist), do: list(&Utils.const/2, dlist)
+
+  def tail(dlist), do: list(&Utils.flipped_const/2, dlist)
+
+  def concat(dlists) do
+    Enum.reduce(dlists, empty(), fn(x, acc) -> append(acc, x) end)
+  end
+
+  defp list(fun, dlist) do
+    case to_list(dlist) do
+      [] -> nil
+      [h | t] -> fun.(h, from_list(t))
+    end
   end
 end
--- test/dlist_test.exs
+++ test/dlist_test.exs
@@ -1,8 +1,67 @@
 defmodule DListTest do
-  use ExUnit.Case
-  doctest DList
+  use ExUnit.Case, async: false
+  use ExCheck
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  property :list_conversion do
+    for_all xs in list(int()) do
+      dlist = DList.from_list(xs)
+
+      DList.to_list(dlist) == xs
+    end
+  end
+
+  property :append do
+    for_all {xs, ys} in {list(int()), list(int())} do
+      dlist_a = DList.from_list(xs)
+      dlist_b = DList.from_list(ys)
+
+      dlist_c = DList.append(dlist_a, dlist_b)
+
+      DList.to_list(dlist_c) == xs ++ ys
+    end
+  end
+
+  property :cons do
+    for_all {xs, x} in {list(int()), int()} do
+      dlist_a = DList.from_list(xs)
+      dlist_b = DList.cons(dlist_a, x)
+
+      DList.to_list(dlist_b) == [x] ++ xs
+    end
+  end
+
+  property :snoc do
+    for_all {xs, x} in {list(int()), int()} do
+      dlist_a = DList.from_list(xs)
+      dlist_b = DList.snoc(dlist_a, x)
+
+      DList.to_list(dlist_b) == xs ++ [x]
+    end
+  end
+
+  property :head do
+    for_all xs in such_that(ys in list(int()) when length(ys) > 0) do
+      dlist = DList.from_list(xs)
+
+      DList.head(dlist) == hd(xs)
+    end
+  end
+
+  property :tail do
+    for_all xs in such_that(ys in list(int()) when length(ys) > 0) do
+      dlist = DList.from_list(xs)
+      tail = dlist |> DList.tail |> DList.to_list
+
+      tail == tl(xs)
+    end
+  end
+
+  property :concat do
+    for_all {xs, ys} in {list(int()), list(int())} do
+      lists = [DList.from_list(xs), DList.from_list(ys)]
+      result = DList.concat(lists)
+
+      DList.to_list(result) == xs ++ ys
+    end
   end
 end
