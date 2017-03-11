4f60d1f1759b4797bba0852a4fa74d6012724992
Juan Soto <juan@juansoto.me>
2017-03-11 11:04:21 -0500

    Add typespecs

--- lib/dlist.ex
+++ lib/dlist.ex
@@ -1,36 +1,48 @@
 defmodule DList do
   @moduledoc false
 
+  @type dlist :: (list -> list)
+
+  @spec from_list(list) :: dlist
   def from_list(xs) do
     fn ys -> xs ++ ys end
   end
 
+  @spec to_list(dlist) :: list
   def to_list(dlist) do
     dlist.([])
   end
 
+  @spec empty() :: dlist
   def empty, do: from_list([])
 
+  @spec singleton(any) :: dlist
   def singleton(x) do
     cons(empty(), x)
   end
 
+  @spec append(dlist, dlist) :: dlist
   def append(dlist_a, dlist_b) do
     Utils.compose(dlist_a, dlist_b)
   end
 
+  @spec cons(dlist, any) :: dlist
   def cons(dlist, x) do
     Utils.compose(Utils.list_cons(x), dlist)
   end
 
+  @spec snoc(dlist, any) :: dlist
   def snoc(dlist, x) do
     Utils.compose(dlist, Utils.list_cons(x))
   end
 
+  @spec head(dlist) :: any
   def head(dlist), do: list(&Utils.const/2, dlist)
 
+  @spec tail(dlist) :: list
   def tail(dlist), do: list(&Utils.flipped_const/2, dlist)
 
+  @spec concat(list(dlist)) :: dlist
   def concat(dlists) do
     Enum.reduce(dlists, empty(), fn(x, acc) -> append(acc, x) end)
   end
--- lib/dlist/utils.ex
+++ lib/dlist/utils.ex
@@ -1,18 +1,25 @@
 defmodule Utils do
   @moduledoc false
 
+  # 1-arity function
+  @type func_one :: (any -> any)
+
+  @spec compose(func_one, func_one) :: func_one
   def compose(f, g) when is_function(g) do
     fn arg -> compose(f, g.(arg)) end
   end
 
+  @spec compose(func_one, any) :: any
   def compose(f, arg) do
     f.(arg)
   end
 
+  @spec list_cons(any) :: (any -> [any])
   def list_cons(x), do: fn(y) -> [x | y] end
 
+  @spec const(any, any) :: any
   def const(a, _b), do: a
 
-
+  @spec flipped_const(any, any) :: any
   def flipped_const(_a, b), do: b
 end
