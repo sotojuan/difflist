b5a52ea436e94e9be26c78fbf8682d02aca06aa4
Juan Soto <juan@juansoto.me>
2017-03-12 09:39:24 -0400

    Add metadata

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule DList.Mixfile do
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -16,11 +18,25 @@ defmodule DList.Mixfile do
     [extra_applications: []]
   end
 
+  defp description do
+    """
+    Difference lists in Elixir
+    """
+  end
+
   defp deps do
     [
       {:excheck, "~> 0.5", only: :test},
       {:triq, github: "triqng/triq", only: :test},
       {:benchfella, "~> 0.3.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["sotojuan"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sotojuan/dlist"}
     ]
   end
 end
